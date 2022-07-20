bbce8bf6ab9f02ce5c98d5bf8212822e9d6a72f5
Javi Velasco <javier.velasco86@gmail.com>
2022-07-20 13:18:17 +0200

    refactor(primitvies): add `ReadableStreamBYOBReader` type

--- packages/primitives/types/streams.d.ts
+++ packages/primitives/types/streams.d.ts
@@ -1,4 +1,16 @@
-import type { ReadableStreamBYOBReader } from 'web-streams-polyfill'
+/**
+ * The type of `ReadableStreamBYOBReader` is not included in Typescript so we
+ * are declaring it inline to not have to worry about bundling.
+ */
+declare class ReadableStreamBYOBReader {
+  constructor(stream: ReadableStream<Uint8Array>)
+  get closed(): Promise<undefined>
+  cancel(reason?: any): Promise<void>
+  read<T extends ArrayBufferView>(
+    view: T
+  ): Promise<{ done: false; value: T } | { done: true; value: T | undefined }>
+  releaseLock(): void
+}
 
 declare const ReadableStreamConstructor: typeof ReadableStream
 declare const ReadableStreamBYOBReaderConstructor: typeof ReadableStreamBYOBReader
