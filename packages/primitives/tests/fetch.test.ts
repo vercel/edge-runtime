0ad9a2aafff5dcd6b61874c5efdaa67653e49fb2
nkzawa <naoyuki.kanezawa@gmail.com>
2022-07-21 15:59:49 +0700

    fix(primitives): support all headers on fetch

--- packages/primitives/tests/fetch.test.ts
+++ packages/primitives/tests/fetch.test.ts
@@ -2,7 +2,19 @@ import { fetch } from '../fetch'
 import { URL } from '../url'
 
 test('sets header calling Headers constructor', async () => {
-  const url = new URL('/about', 'https://vercel.com');
+  const url = new URL('/about', 'https://vercel.com')
   const response = await fetch(url)
+  expect(response.status).toEqual(200)
+})
+
+test('sets headers unsupported in undici', async () => {
+  const url = new URL('/', 'https://example.com')
+  const response = await fetch(url, {
+    headers: {
+      Connection: 'keep-alive',
+      'Keep-Alive': 'timeout=5, max=1000',
+      'Transfer-Encoding': 'gzip',
+    },
+  })
   expect(response.status).toEqual(200)
 })
