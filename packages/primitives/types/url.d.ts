77af2ac8fee118ec64597c9e9bd1f3412b5b07ab
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-07-26 15:55:24 +0200

    fix(primitives): typeof for constructors

--- packages/primitives/types/url.d.ts
+++ packages/primitives/types/url.d.ts
@@ -1,8 +1,8 @@
 import type { URLPattern } from 'urlpattern-polyfill/dist/types'
 
 declare const URLPatternConstructor: typeof URLPattern
-declare const URLConstructor: URL
-declare const URLSearchParamsConstructor: URLSearchParams
+declare const URLConstructor: typeof URL
+declare const URLSearchParamsConstructor: typeof URLSearchParams
 
 export { URLPatternConstructor as URLPattern }
 export { URLConstructor as URL }
