15e363909b46e37f03eb34a53f1ec2060b149ec7
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-19 10:12:09 +0200

    build: use tsconfig.prod

--- packages/format/tsup.config.ts
+++ packages/format/tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
   dts: true,
+  entry: ['./src/index.ts'],
   format: ['cjs', 'esm'],
   tsconfig: './tsconfig.prod.json',
 })
