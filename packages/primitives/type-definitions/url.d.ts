5e4fbc115676d6647f8be6f83a099f55dd12b81f
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-22 11:29:56 +0200

    refactor(primitives): use dependency types

--- packages/primitives/type-definitions/url.d.ts
+++ packages/primitives/type-definitions/url.d.ts
@@ -1,54 +1,11 @@
-type URLPatternInput = URLPatternInit | string
-
-declare class URLPattern {
-  constructor(init?: URLPatternInput, baseURL?: string)
-  test(input?: URLPatternInput, baseURL?: string): boolean
-  exec(input?: URLPatternInput, baseURL?: string): URLPatternResult | null
-  readonly protocol: string
-  readonly username: string
-  readonly password: string
-  readonly hostname: string
-  readonly port: string
-  readonly pathname: string
-  readonly search: string
-  readonly hash: string
-}
-
-interface URLPatternInit {
-  baseURL?: string
-  username?: string
-  password?: string
-  protocol?: string
-  hostname?: string
-  port?: string
-  pathname?: string
-  search?: string
-  hash?: string
-}
-
-interface URLPatternResult {
-  inputs: [URLPatternInput]
-  protocol: URLPatternComponentResult
-  username: URLPatternComponentResult
-  password: URLPatternComponentResult
-  hostname: URLPatternComponentResult
-  port: URLPatternComponentResult
-  pathname: URLPatternComponentResult
-  search: URLPatternComponentResult
-  hash: URLPatternComponentResult
-}
-
-interface URLPatternComponentResult {
-  input: string
-  groups: {
-    [key: string]: string | undefined
-  }
-}
+import type { URLPattern } from 'urlpattern-polyfill/dist/types'
 
 declare const URLPatternConstructor: typeof URLPattern
 declare const URLConstructor: typeof URL
 declare const URLSearchParamsConstructor: typeof URLSearchParams
 
-export { URLPatternConstructor as URLPattern }
-export { URLConstructor as URL }
-export { URLSearchParamsConstructor as URLSearchParams }
+export {
+  URLConstructor as URL,
+  URLPatternConstructor as URLPattern,
+  URLSearchParamsConstructor as URLSearchParams,
+}
