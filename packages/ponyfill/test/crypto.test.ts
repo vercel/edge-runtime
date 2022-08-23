d4807aadca18aa87ced040a3174b381479963909
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-23 13:25:05 +0200

    test(ponyfill): better code comment

--- packages/ponyfill/test/crypto.test.ts
+++ packages/ponyfill/test/crypto.test.ts
@@ -15,8 +15,9 @@ test('crypto.subtle digest', async () => {
     .map((b) => b.toString(16).padStart(2, '0'))
     .join('')
 
+  // this value is obtained from
+  // require('crypto').createHash('sha256').update('Hello, world').digest('hex')
   const expected =
-    // require('crypto').createHash('sha256').update('Hello, world').digest('hex')
     '4ae7c3b6ac0beff671efa8cf57386151c06e58ca53a78d83f36107316cec125f'
 
   expect(hex).toEqual(expected)
