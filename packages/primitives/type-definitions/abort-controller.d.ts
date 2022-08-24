7b183d8c90b669e3b94fb4cde9f59ea793a05da0
feugy <damien@vercel.com>
2022-08-24 15:40:33 +0200

    fix(runtime): invalid typings

--- packages/primitives/type-definitions/abort-controller.d.ts
+++ packages/primitives/type-definitions/abort-controller.d.ts
@@ -1,18 +1,12 @@
-declare const AbortControllerConstructor: typeof AbortController
-declare const AbortSignalConstructor: typeof AbortSignal
-declare const DOMExceptionConstructor: typeof DOMException
+declare const AbortControllerConstructor: AbortController
 
-declare var WathwgAbortSignal: {
-  prototype: AbortSignalConstructor
-  new (): AbortSignalConstructor
+declare var AbortSignal: {
+  prototype: typeof AbortSignal
+  new (): typeof AbortSignal
   /** Returns an AbortSignal instance which will be aborted in milliseconds milliseconds. Its abort reason will be set to a "TimeoutError" DOMException. */
   timeout(milliseconds: number): AbortSignal
   /** Returns an AbortSignal instance whose abort reason is set to reason if not undefined; otherwise to an "AbortError" DOMException. */
   abort(reason?: string): AbortSignal
 }
 
-export {
-  AbortControllerConstructor as AbortController,
-  DOMExceptionConstructor as DOMException,
-  WathwgAbortSignal as AbortSignal,
-}
+export { AbortControllerConstructor as AbortController, AbortSignal }
