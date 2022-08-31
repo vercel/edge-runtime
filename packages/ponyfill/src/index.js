27d186c9bb1622d45d2970a13fd88e371c8bc7c9
Gal Schlezinger <gal@spitfire.co.il>
2022-08-31 11:12:50 +0300

    Only export specific types for Edge Runtime

--- packages/ponyfill/src/index.js
+++ packages/ponyfill/src/index.js
@@ -1,2 +1,42 @@
 module.exports =
-  typeof EdgeRuntime === 'string' ? self : require('@edge-runtime/primitives')
+  typeof EdgeRuntime === 'string' ? edge() : require('@edge-runtime/primitives')
+
+function edge() {
+  return {
+    AbortController,
+    AbortSignal,
+    Blob,
+    Cache,
+    CacheStorage,
+    Crypto,
+    CryptoKey,
+    Event,
+    EventTarget,
+    FetchEvent,
+    File,
+    FormData,
+    Headers,
+    PromiseRejectionEvent,
+    ReadableStream,
+    ReadableStreamBYOBReader,
+    ReadableStreamDefaultReader,
+    Request,
+    Response,
+    SubtleCrypto,
+    TextDecoder,
+    TextEncoder,
+    TransformStream,
+    URL,
+    URLPattern,
+    URLSearchParams,
+    WritableStream,
+    WritableStreamDefaultWriter,
+    atob,
+    btoa,
+    caches,
+    console,
+    crypto,
+    fetch,
+    structuredClone,
+  }
+}
---
+++ packages/ponyfill/test/compliance-with-primitives.node.test.ts
@@ -0,0 +1,73 @@
+import { parse } from 'acorn-loose'
+import { promises as fs } from 'fs'
+import { simple } from 'acorn-walk'
+import { EdgeVM } from '@edge-runtime/vm'
+
+test('exports all primitives in Edge Runtime', async () => {
+  const exportedNames = await getExportedNames()
+
+  const anyObject = exportedNames.reduce(
+    (acc: Record<string, any>, name: string) => {
+      acc[name] = expect.anything()
+      return acc
+    },
+    {}
+  )
+
+  const runtime = new EdgeVM({
+    codeGeneration: { strings: false, wasm: false },
+  })
+  const result = runtime.require(require.resolve('..'))
+
+  for (const key of LIMBO_STATE) {
+    delete anyObject[key]
+    delete result[key]
+  }
+
+  expect(result).toEqual(anyObject)
+})
+
+test('exports all primitives in Node.js', async () => {
+  const exportedNames = await getExportedNames()
+
+  const anyObject = exportedNames.reduce(
+    (acc: Record<string, any>, name: string) => {
+      acc[name] = expect.anything()
+      return acc
+    },
+    {}
+  )
+
+  const result = { ...require('..') }
+
+  for (const key of LIMBO_STATE) {
+    delete anyObject[key]
+    delete result[key]
+  }
+
+  expect(result).toEqual(anyObject)
+})
+
+async function getExportedNames() {
+  const typesPath = require.resolve('@edge-runtime/primitives/types/index.d.ts')
+  const typesContents = await fs.readFile(typesPath, 'utf8')
+  const ast = parse(typesContents, { ecmaVersion: 'latest' })
+  const exportedNames: string[] = []
+  simple(ast, {
+    ExportNamedDeclaration(node: any) {
+      for (const specifier of node.specifiers) {
+        if (specifier.exported?.name) {
+          exportedNames.push(specifier.exported.name)
+        }
+      }
+    },
+  })
+  return exportedNames
+}
+
+export const LIMBO_STATE = [
+  'createCaches',
+  'DOMException',
+  'setGlobalDispatcher',
+  'getGlobalDispatcher',
+]
