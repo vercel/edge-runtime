849d53b718d2bacfbd6fbb0ea2e988cf69aa928d
Balázs Orbán <info@balazsorban.com>
2022-10-07 17:02:54 +0200

    add swc transform to tests

--- packages/jest-environment/jest.config.js
+++ packages/jest-environment/jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   testEnvironment: './dist',
   setupFilesAfterEnv: ['./jest.setup'],
+  transform: {
+    '\\.(js|jsx|ts|tsx)$': [
+      '@swc/jest',
+      /** @type {import("@swc/core").Config} */
+      { jsc: { parser: { syntax: 'typescript' } } },
+    ],
+  },
 }
