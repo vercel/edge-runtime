0f8977862addeb54c83e527f76dab807cb5a8b8a
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-11-04 12:22:26 +0100

    refactor: move common code out of function scope

--- packages/jest-environment/src/extend/response.ts
+++ packages/jest-environment/src/extend/response.ts
@@ -7,6 +7,16 @@ function assertResponse(actual: unknown): asserts actual is Response {
   throw new Error('Expected a Response instance')
 }
 
+const HTTP_STATUS_CODE_RANGES = {
+  '1': 'Informational',
+  '2': 'Successful',
+  '3': 'Redirection',
+  '4': 'Client Error',
+  '5': 'Server Error',
+}
+
+type HttpStatusCodeRange = keyof typeof HTTP_STATUS_CODE_RANGES
+
 // Matchers
 
 expect.extend({
@@ -14,19 +24,11 @@ expect.extend({
     assertResponse(actual)
     const [status] = args
     if (typeof status === 'string') {
-      const ranges = {
-        '1': 'Informational',
-        '2': 'Successful',
-        '3': 'Redirection',
-        '4': 'Client Error',
-        '5': 'Server Error',
-      }
-      type RangeType = keyof typeof ranges
-      const range = actual.status.toString()[0] as RangeType
-      const matchedRange = ranges[range]
-
-      const statusRange = Object.keys(ranges).find(
-        (k) => ranges[k as RangeType] === status
+      const httpStatusCodeRange =
+        actual.status.toString()[0] as HttpStatusCodeRange
+      const matchedRange = HTTP_STATUS_CODE_RANGES[httpStatusCodeRange]
+      const statusRange = Object.keys(httpStatusCodeRange).find(
+        (k) => httpStatusCodeRange[k as HttpStatusCodeRange] === status
       )
 
       const pass = matchedRange === status
