74bf68588a4d40ecac374457c7b05a8091887346
Balázs Orbán <info@balazsorban.com>
2022-11-04 21:34:29 +0100

    Delete jest.config.ts

--- packages/jest-environment/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import buildConfig from '../../jest.config'
-import type { Config } from '@jest/types'
-
-const config: Config.InitialOptions = buildConfig(__dirname)
-config.testEnvironment = './dist'
-export default config
