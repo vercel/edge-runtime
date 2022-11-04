5e9d8eb722ccc9dcf98ad5c7e6124a0223d20cf5
Balázs Orbán <info@balazsorban.com>
2022-11-04 11:01:03 +0100

    format tests

--- packages/jest-environment/test/index.test.ts
+++ packages/jest-environment/test/index.test.ts
@@ -17,33 +17,35 @@ test('has EdgeRuntime global', () => {
 })
 
 describe('Custom matchers', () => {
-  test('`expect.toHaveStatus` available', () => {
-    const okResponse = new Response('OK')
+  describe.only('Response matchers', () => {
+    test('`expect.toHaveStatus` available', () => {
+      const okResponse = new Response('OK')
 
-    expect(okResponse).toHaveStatus(200)
-    expect(okResponse).toHaveStatus('Successful')
-    expect(okResponse).not.toHaveStatus(201)
+      expect(okResponse).toHaveStatus(200)
+      expect(okResponse).toHaveStatus('Successful')
+      expect(okResponse).not.toHaveStatus(201)
 
-    expect(new Response('Internal Server Error', { status: 500 })).toHaveStatus(
-      'Server Error'
-    )
-  })
+      expect(
+        new Response('Internal Server Error', { status: 500 })
+      ).toHaveStatus('Server Error')
+    })
 
-  test('`expect.toHaveJSONBody` available', async () => {
-    await expect(
-      expect(new Response('Without Content-Type')).toHaveJSONBody(null)
-    ).rejects.toThrowErrorMatchingInlineSnapshot(`
-      "[2mexpect([22m[31mreceived[39m[2m).[22mtoHaveJSONBody[2m([22m[32mexpected[39m[2m)[22m
+    test('`expect.toHaveJSONBody` available', async () => {
+      await expect(
+        expect(new Response('Without Content-Type')).toHaveJSONBody(null)
+      ).rejects.toThrowErrorMatchingInlineSnapshot(`
+        "[2mexpect([22m[31mreceived[39m[2m).[22mtoHaveJSONBody[2m([22m[32mexpected[39m[2m)[22m
 
-      Expected response to have \\"Content-Type\\": [32m\\"application/json\\"[39m
-      Received: [31m\\"text/plain;charset=UTF-8\\"[39m"
-    `)
+        Expected response to have \\"Content-Type\\": [32m\\"application/json\\"[39m
+        Received: [31m\\"text/plain;charset=UTF-8\\"[39m"
+      `)
 
-    const json = { foo: 'bar' }
-    // @ts-expect-error See https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-    const response = Response.json(json)
+      const json = { foo: 'bar' }
+      // @ts-expect-error See https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+      const response = Response.json(json)
 
-    await expect(response).toHaveJSONBody(json)
-    await expect(response).not.toHaveJSONBody({ foo: 'baz' })
+      await expect(response).toHaveJSONBody(json)
+      await expect(response).not.toHaveJSONBody({ foo: 'baz' })
+    })
   })
 })
