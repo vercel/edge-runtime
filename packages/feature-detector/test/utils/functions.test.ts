3fbfdc457e1a3a1830d5baf214f1ccf36abfce89
feugy <damien@vercel.com>
2023-01-09 09:47:56 +0100

    chore(feature-detector): adds more test cases

--- packages/feature-detector/test/utils/functions.test.ts
+++ packages/feature-detector/test/utils/functions.test.ts
@@ -5,8 +5,8 @@ const { jsFile, tsFile } = initTestProjectAndFiles()
 
 describe.each([
   { title: 'for JavaScript', file: jsFile },
-  { title: 'for TypeScript', file: tsFile },
-])('getReturnType() $title', ({ file }) => {
+  { title: 'for TypeScript', file: tsFile, isTS: true },
+])('getReturnType() $title', ({ file, isTS }) => {
   it('finds DOM types', () => {
     file.replaceWithText(`function foo() { return new Response('ok') }`)
     const fct = file.getFunctionOrThrow('foo')
@@ -38,4 +38,28 @@ describe.each([
     const fct = file.getDefaultExportSymbol()?.getDeclarations()[0]
     expect(getReturnType(fct)?.getText()).toBe('number')
   })
+
+  it('handles cyclic references', () => {
+    file.replaceWithText(`
+      const exported = () => { 
+        const x = {}
+        x.next = x
+        return x.next;
+      }`)
+    const fct = file
+      .getVariableStatementOrThrow('exported')
+      .getDeclarations()[0]
+      .getChildAtIndex(2)
+    expect(getReturnType(fct)?.getText()).toBe(isTS ? 'any' : '{ next: any; }')
+  })
+
+  if (isTS) {
+    it('handles invalid cyclic type', () => {
+      file.replaceWithText(`
+        type A = A
+        function foo(): A {}`)
+      const fct = file.getFunctionOrThrow('foo')
+      expect(getReturnType(fct)?.getText()).toBe('any')
+    })
+  }
 })
