4170694c5bec1c2f2339717665088d9d4591b171
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-01-26 12:19:17 +0100

    docs: remove unnecessary settings (#242)

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,10 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  flexsearch: {
-    codeblocks: false,
-  },
-  unstable_staticImage: true,
   defaultShowCopyCode: true,
 })
 
