4882e08e3be8cff02e576df7c0b71222698fc8f1
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-27 19:42:19 +0100

    test(primitives). use a local server for POST

--- packages/primitives/tests/fetch.test.ts
+++ packages/primitives/tests/fetch.test.ts
@@ -1,8 +1,36 @@
+import { Server, createServer, IncomingMessage, ServerResponse } from 'http'
+// @ts-ignore package `http-body` doesn't export type
+import * as httpBody from 'http-body'
+import listen from 'test-listen'
+import multer from 'multer'
+
 import { FormData, fetch } from '../fetch'
 import { URL } from '../url'
 
+let server: Server
+afterEach(() => new Promise((resolve) => server.close(resolve)))
+
 test('perform a POST as application/json', async () => {
-  const response = await fetch('https://httpbin.org/post', {
+  server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.method !== 'POST') {
+      res.statusCode = 400
+      res.end()
+    }
+
+    if (req.headers['content-type'] === 'application/json') {
+      const text = await httpBody.text(req)
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Content-Length', Buffer.byteLength(text))
+      res.end(text)
+      return
+    }
+    res.statusCode = 400
+    res.end()
+  })
+
+  const serverUrl = await listen(server)
+  const response = await fetch(serverUrl, {
     method: 'POST',
     body: JSON.stringify({ foo: 'bar' }),
     headers: {
@@ -11,13 +39,32 @@ test('perform a POST as application/json', async () => {
   })
 
   expect(response.status).toBe(200)
-  const json = await response.json()
-  expect(JSON.parse(json.data)).toEqual({ foo: 'bar' })
-  expect(json.headers['Content-Type']).toBe('application/json')
+  expect(await response.json()).toEqual({ foo: 'bar' })
 })
 
 test('perform a POST as application/x-www-form-urlencoded', async () => {
-  const response = await fetch('https://httpbin.org/post', {
+  server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.method !== 'POST') {
+      res.statusCode = 400
+      res.end()
+    }
+
+    if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+      const text = await httpBody.text(req)
+      const urlSearchParam = new URLSearchParams(text)
+      const urlEncoded = JSON.stringify(
+        Object.fromEntries(urlSearchParam.entries())
+      )
+      res.statusCode = 200
+      res.end(urlEncoded)
+      return
+    }
+    res.statusCode = 400
+    res.end()
+  })
+
+  const serverUrl = await listen(server)
+  const response = await fetch(serverUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -27,24 +74,43 @@ test('perform a POST as application/x-www-form-urlencoded', async () => {
 
   expect(response.status).toBe(200)
   const json = await response.json()
-  expect(json.form).toEqual({ foo: 'bar' })
-  expect(json.headers['Content-Type']).toBe('application/x-www-form-urlencoded')
+  expect(json).toEqual({ foo: 'bar' })
 })
 
 test('perform a POST as multipart/form-data', async () => {
+  const upload = multer({ storage: multer.memoryStorage() })
+  server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (
+      req.method !== 'POST' ||
+      !req.headers['content-type']?.startsWith('multipart/form-data')
+    ) {
+      res.statusCode = 400
+      res.end()
+    }
+    upload.none()(req as any, res as any, () => {
+      res.statusCode = 200
+      // @ts-expect-error
+      res.end(JSON.stringify(req.body))
+      return
+    })
+  })
+
+  const serverUrl = await listen(server)
+
   const formData = new FormData()
   formData.append('company', 'vercel')
   formData.append('project', 'edge-runtime')
 
-  const response = await fetch('https://httpbin.org/post', {
+  const response = await fetch(serverUrl, {
     method: 'POST',
     body: formData,
   })
 
   expect(response.status).toBe(200)
-  const json = await response.json()
-  expect(json.form).toEqual({ company: 'vercel', project: 'edge-runtime' })
-  expect(json.headers['Content-Type']).toContain('multipart/form-data')
+  expect(await response.json()).toEqual({
+    company: 'vercel',
+    project: 'edge-runtime',
+  })
 })
 
 test('sets header calling Headers constructor', async () => {
