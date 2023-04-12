304040d087e450e93ddaa6f812c00fb15d600cb0
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-04-12 16:18:16 +0200

    fix: inline type statement (#306)
    * fix: inline type statement
    
    * Create cyan-teachers-burn.md

--- packages/runtime/src/server/index.ts
+++ packages/runtime/src/server/index.ts
@@ -1,3 +1,3 @@
 export { consumeUint8ArrayReadableStream } from './body-streams'
 export { createHandler } from './create-handler'
-export { runServer, type EdgeRuntimeServer } from './run-server'
+export { runServer, EdgeRuntimeServer } from './run-server'
