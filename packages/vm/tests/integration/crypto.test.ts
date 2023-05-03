32c10c6709a5fea73e84d215958be26d2bb881d5
Gal Schlezinger <gal@spitfire.co.il>
2023-05-03 17:46:46 +0300

    Add a test to check uint8array for crypto

--- packages/vm/tests/integration/crypto.test.ts
+++ packages/vm/tests/integration/crypto.test.ts
@@ -1,4 +1,5 @@
 import { EdgeVM } from '../../src'
+import { createHash } from 'crypto'
 
 test('crypto.subtle.digest returns an ArrayBuffer', async () => {
   const vm = new EdgeVM()
@@ -16,3 +17,26 @@ test('crypto.subtle.digest returns an ArrayBuffer', async () => {
   const digest = await vm.evaluate(`(${fn})()`)
   expect(digest).toBeInstanceOf(fromContext.ArrayBuffer)
 })
+
+test('crypto.subtle.digest returns a SHA-256 hash', async () => {
+  const vm = new EdgeVM()
+
+  async function fn() {
+    const digest = await crypto.subtle.digest(
+      'SHA-256',
+      new Uint8Array([104, 105, 33])
+    )
+    return digest
+  }
+
+  const digest = await vm.evaluate(`(${fn})()`)
+  expect(toHex(digest)).toEqual(
+    createHash('sha256').update('hi!').digest('hex')
+  )
+})
+
+function toHex(buffer: ArrayBuffer) {
+  return Array.from(new Uint8Array(buffer))
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('')
+}
