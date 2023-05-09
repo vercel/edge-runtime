7e1adcec6ed636dcb71bc34f384e559ad369c9e1
Gal Schlezinger <gal@spitfire.co.il>
2023-05-09 16:15:55 +0300

    fix tests in Node.js 20

--- packages/primitives/src/primitives/load.js
+++ packages/primitives/src/primitives/load.js
@@ -177,6 +177,7 @@ export function load(scopedContext = {}) {
       ['streams', { exports: streamsImpl }],
     ]),
     scopedContext: {
+      global: context,
       ...scopedContext,
       ...streamsImpl,
       ...urlImpl,
