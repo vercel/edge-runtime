52ac453d3ab04e9582849093f89e7639721366a9
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-06-01 11:41:12 +0200

    refactor(primites): avoid unnecessary declaration

--- packages/primitives/src/primitives/encoding.js
+++ packages/primitives/src/primitives/encoding.js
@@ -1,7 +1,2 @@
 export const atob = (enc) => Buffer.from(enc, 'base64').toString('binary')
 export const btoa = (str) => Buffer.from(str, 'binary').toString('base64')
-
-const TE = TextEncoder
-const TD = TextDecoder
-
-export { TE as TextEncoder, TD as TextDecoder }
