90258da58bee3e3fe82386c60ec736cd4118477f
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-17 18:30:21 +0200

    chore: unify splitCookiesString implementation
    closes #257

--- packages/cookies/src/index.ts
+++ packages/cookies/src/index.ts
@@ -1,3 +1,4 @@
 export type { CookieListItem, RequestCookie, ResponseCookie } from './types'
 export { RequestCookies } from './request-cookies'
 export { ResponseCookies } from './response-cookies'
+export { splitCookiesString } from './serialize'
