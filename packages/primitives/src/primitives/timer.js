5a92a9bb478a54d02d28eddf74d79022bf3c397a
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2023-10-03 04:22:35 +0900

    fix timers proxy

--- packages/primitives/src/primitives/timer.js
+++ packages/primitives/src/primitives/timer.js
@@ -1,4 +1,4 @@
-const timeoutProxy = new Proxy(setTimeout, {
+const setTimeoutProxy = new Proxy(setTimeout, {
   apply: (target, thisArg, args) => {
     const timeout = Reflect.apply(target, thisArg, args)
     // Returns integer value of timeout ID
@@ -6,5 +6,13 @@ const timeoutProxy = new Proxy(setTimeout, {
   },
 })
 
-export { timeoutProxy as setTimeout }
-export { timeoutProxy as setInterval }
+const setIntervalProxy = new Proxy(setInterval, {
+  apply: (target, thisArg, args) => {
+    const timeout = Reflect.apply(target, thisArg, args)
+    // Returns integer value of timeout ID
+    return timeout[Symbol.toPrimitive]()
+  },
+})
+
+export { setTimeoutProxy as setTimeout }
+export { setIntervalProxy as setInterval }
