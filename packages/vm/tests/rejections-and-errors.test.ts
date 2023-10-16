37a53ae50e92e936cdce145d55c59c2ed5ba8d72
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-16 19:48:57 +0200

    test: remove node16 conditional cases

--- packages/vm/tests/rejections-and-errors.test.ts
+++ packages/vm/tests/rejections-and-errors.test.ts
@@ -42,13 +42,6 @@ it('does not swallow unhandled rejections outside of evaluation', async () => {
       )}`,
       {
         encoding: 'utf8',
-        env: process.version.startsWith('v14')
-          ? {
-              ...process.env,
-              // node 14 does not throw on unhandled rejections, so this test would resolve without the flag
-              'NODE_OPTIONS': '--unhandled-rejections=strict',
-            }
-          : undefined,
       },
     ),
   ).rejects.toThrow(/intentional break/)
