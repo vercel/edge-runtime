aef04d5b5ecaf1e3ff8a6f01d688eba7ac2d54f7
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2023-10-24 02:09:01 +0900

    fix timers primitive types

--- packages/primitives/type-definitions/timers.d.ts
+++ packages/primitives/type-definitions/timers.d.ts
@@ -1,5 +1,5 @@
-declare const _setTimeout: typeof Number
-declare const _setInterval: typeof Number
+declare const _setTimeout: typeof setTimeout
+declare const _setInterval: typeof setInterval
 
 export { _setTimeout as setTimeout }
 export { _setInterval as setInterval }
