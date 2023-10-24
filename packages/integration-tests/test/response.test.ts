14d5942db719531ba0a364ed2971599fe177dc80
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-24 18:46:25 +0200

    test: skip in non edge

--- packages/integration-tests/test/response.test.ts
+++ packages/integration-tests/test/response.test.ts
@@ -39,33 +39,31 @@ testOrSkip('allow to set `set-cookie` header', async () => {
   response.headers.set('set-cookie', 'foo=bar')
   expect(response.headers.get('set-cookie')).toEqual('foo=bar')
 })
+;(globalThis.EdgeRuntime !== undefined ? testOrSkip : test.skip)(
+  'allow to append multiple `set-cookie` header',
+  async () => {
+    const response = new Response(null)
+    response.headers.append('set-cookie', 'foo=bar')
+    response.headers.append('set-cookie', 'bar=baz')
 
-testOrSkip('allow to append multiple `set-cookie` header', async () => {
-  const response = new Response(null)
-  response.headers.append('set-cookie', 'foo=bar')
-  response.headers.append('set-cookie', 'bar=baz')
+    expect(response.headers.getSetCookie()).toEqual(['foo=bar', 'bar=baz'])
 
-  expect(response.headers.getSetCookie()).toEqual(['foo=bar', 'bar=baz'])
-
-  expect(response.headers.getAll?.('set-cookie')).toEqual([
-    'foo=bar',
-    'bar=baz',
-  ])
-})
+    expect(response.headers.getAll?.('set-cookie')).toEqual([
+      'foo=bar',
+      'bar=baz',
+    ])
+  },
+)
 
 testOrSkip('disallow mutate response headers for redirects', async () => {
   const response = Response.redirect('https://edge-ping.vercel.app/')
   expect(() => response.headers.set('foo', 'bar')).toThrow('immutable')
 })
-
-testOrSkip('allow to mutate response headers for error', async () => {
-  const response = Response.error()
-  response.headers.set('foo', 'bar')
-  expect(response.headers.get('foo')).toEqual('bar')
-})
-
-testOrSkip('allow to mutate response headers', async () => {
-  const response = await fetch('https://edge-ping.vercel.app/')
-  response.headers.set('foo', 'bar')
-  expect(response.headers.get('foo')).toEqual('bar')
-})
+;(globalThis.EdgeRuntime !== undefined ? testOrSkip : test.skip)(
+  'allow to mutate response headers for error',
+  async () => {
+    const response = Response.error()
+    response.headers.set('foo', 'bar')
+    expect(response.headers.get('foo')).toEqual('bar')
+  },
+)
