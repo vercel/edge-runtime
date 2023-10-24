9c42276cb74607e97695f29b0e07118bc45a23a6
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-10-24 18:34:27 +0200

    test: skip in node16

--- packages/integration-tests/test/crypto.test.ts
+++ packages/integration-tests/test/crypto.test.ts
@@ -4,6 +4,9 @@ if (!globalThis.crypto) {
   globalThis.crypto = require('crypto')
 }
 
+const testOrSkip =
+  process.versions.node.split('.').map(Number)[0] > 16 ? test : test.skip
+
 function toHex(buffer: ArrayBuffer) {
   return Array.from(new Uint8Array(buffer))
     .map((b) => b.toString(16).padStart(2, '0'))
@@ -14,7 +17,7 @@ test('crypto.randomUUID', async () => {
   expect(crypto.randomUUID()).toEqual(expect.stringMatching(/^[a-f0-9-]+$/))
 })
 
-test('crypto.subtle.digest returns a SHA-256 hash', async () => {
+testOrSkip('crypto.subtle.digest returns a SHA-256 hash', async () => {
   const digest = await crypto.subtle.digest(
     'SHA-256',
     new Uint8Array([104, 105, 33]),
@@ -24,7 +27,7 @@ test('crypto.subtle.digest returns a SHA-256 hash', async () => {
   )
 })
 
-test('Ed25519', async () => {
+testOrSkip('Ed25519', async () => {
   const kp = await crypto.subtle.generateKey('Ed25519', false, [
     'sign',
     'verify',
@@ -33,7 +36,7 @@ test('Ed25519', async () => {
   expect(kp).toHaveProperty('publicKey')
 })
 
-test('X25519', async () => {
+testOrSkip('X25519', async () => {
   const kp = await crypto.subtle.generateKey('X25519', false, [
     'deriveBits',
     'deriveKey',
